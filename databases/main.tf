2e63a5e1452412b537a7883ee76bd01c3d98527d
Pedro Duarte <pedro.ti@live.com>
2023-11-09 16:10:24 -0300

    feat: config database

--- databases/main.tf
+++ databases/main.tf
@@ -2,6 +2,35 @@ provider "kubernetes" {
   config_path = "~/.kube/config"
 }
 
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "postgres" {
+  name       = "my-postgres"
+  chart      = "postgresql"
+  repository = "https://charts.bitnami.com/bitnami"
+  namespace  = var.namespace
+  set {
+    name  = "postgresPassword"
+    value = "ModeloXPTO"
+  }
+}
+
+resource "helm_release" "redis" {
+  name       = "my-redis"
+  chart      = "redis"
+  repository = "https://charts.bitnami.com/bitnami"
+  namespace  = var.namespace
+  set {
+    name  = "redisPassword"
+    value = "XPTOioio"
+  }
+}
+
+
 resource "kubernetes_namespace" "db_namespace" {
   metadata {
     name = var.namespace
