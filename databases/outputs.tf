5c21b7d9605fab6607cf8ec3d16187c005dba46d
Pedro Duarte <pedro.ti@live.com>
2023-11-09 16:15:14 -0300

    feat: ommited output

--- databases/outputs.tf
+++ databases/outputs.tf
@@ -1,7 +1,7 @@
-output "postgresql_external_ip" {
-  value = module.postgresql.external_ip
-}
+# output "postgresql_external_ip" {
+#   value = module.postgresql.external_ip
+# }
 
-output "redis_external_ip" {
-  value = module.redis.master_service[0].load_balancer[0].ingress[0].ip
-}
+# output "redis_external_ip" {
+#   value = module.redis.master_service[0].load_balancer[0].ingress[0].ip
+# }
