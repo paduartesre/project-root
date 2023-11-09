e106e48be42d6a119d9df7d024a005a4ef9ab696
Pedro Duarte <pedro.ti@live.com>
2023-11-09 16:11:46 -0300

    fix: fix modules

--- databases/main.tf
+++ databases/main.tf
@@ -8,6 +8,12 @@ provider "helm" {
   }
 }
 
+resource "kubernetes_namespace" "db_namespace" {
+  metadata {
+    name = var.namespace
+  }
+}
+
 resource "helm_release" "postgres" {
   name       = "my-postgres"
   chart      = "postgresql"
@@ -27,49 +33,5 @@ resource "helm_release" "redis" {
   set {
     name  = "redisPassword"
     value = "XPTOioio"
-  }
-}
-
-
-resource "kubernetes_namespace" "db_namespace" {
-  metadata {
-    name = var.namespace
-  }
-}
-
-module "postgresql" {
-  source  = "bitnami/postgresql/kubernetes"
-  version = "1.0.0"
-
-  namespace       = kubernetes_namespace.db_namespace.metadata[0].name
-  database        = var.postgres_db_name
-  username        = var.postgres_user
-  password        = var.postgres_password
-  volume_mounts   = [{
-    mount_path = "/bitnami/postgresql",
-    name       = "data",
-  }]
-  volume_claims   = [{
-    access_modes = ["ReadWriteOnce"]
-    resources = {
-      requests = {
-        storage = "8Gi"
-      }
-    }
-  }]
-}
-
-module "redis" {
-  source  = "bitnami/redis/kubernetes"
-  version = "1.0.0"
-
-  namespace = kubernetes_namespace.db_namespace.metadata[0].name
-  cluster_enabled = true
-  password = var.redis_password
-  master = {
-    persistence = {
-      enabled = true
-      size = "8Gi"
-    }
   }
 }
