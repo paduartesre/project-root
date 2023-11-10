5cd312e0c28c927532ea42e7b69ce18d1b16c274
Ubuntu <ubuntu@ip-172-31-41-4.sa-east-1.compute.internal>
2023-11-10 00:01:30 +0000

    feat: Deploy Redis and PGSQL

--- databases/main.tf
+++ databases/main.tf
@@ -2,72 +2,171 @@ provider "kubernetes" {
   config_path = "~/.kube/config"
 }
 
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
+resource "kubernetes_namespace" "databases" {
+  metadata {
+    name = "databases"
   }
 }
 
-resource "helm_release" "postgres" {
-  name       = "my-postgres"
-  chart      = "postgresql"
-  repository = "https://charts.bitnami.com/bitnami"
-  namespace  = var.namespace_db
-
-  set {
-    name  = "postgresPassword"
-    value = "ModeloXPTO"
+# PostgreSQL Deployment
+resource "kubernetes_deployment" "postgres" {
+  metadata {
+    name = "postgres"
+    namespace = kubernetes_namespace.databases.metadata[0].name
   }
 
-  set {
-    name  = "resources.requests.memory"
-    value = "128Mi"
-  }
+  spec {
+    replicas = 1
 
-  set {
-    name  = "resources.requests.cpu"
-    value = "200m"
-  }
+    selector {
+      match_labels = {
+        app = "postgres"
+      }
+    }
 
-  set {
-    name  = "resources.limits.memory"
-    value = "512Mi"
-  }
+    template {
+      metadata {
+        labels = {
+          app = "postgres"
+        }
+      }
 
-  set {
-    name  = "resources.limits.cpu"
-    value = "500m"
+      spec {
+        container {
+          image = "postgres:alpine3.18"
+          name  = "postgres"
+
+          env {
+            name  = "POSTGRES_DB"
+            value = "db_wordpress"
+          }
+
+          env {
+            name  = "POSTGRES_USER"
+            value = "db_user"
+          }
+
+          env {
+            name  = "POSTGRES_PASSWORD"
+            value = "XPTOPoio00*9"
+          }
+
+          volume_mount {
+            mount_path = "/var/lib/postgresql/data"
+            name       = "postgres-storage"
+          }
+
+          resources {
+            requests = {
+              cpu    = "500m"
+              memory = "512Mi"
+            }
+            limits = {
+              cpu    = "1000m"
+              memory = "1024Mi"
+            }
+          }
+        }
+
+        volume {
+          name = "postgres-storage"
+
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim.postgres_pvc.metadata[0].name
+          }
+        }
+      }
+    }
   }
 }
 
-resource "helm_release" "redis" {
-  name       = "my-redis"
-  chart      = "redis"
-  repository = "https://charts.bitnami.com/bitnami"
-  namespace  = var.namespace_db
-
-  set {
-    name  = "redisPassword"
-    value = "XPTOioio"
+# Redis Deployment
+resource "kubernetes_deployment" "redis" {
+  metadata {
+    name = "redis"
+    namespace = kubernetes_namespace.databases.metadata[0].name
   }
 
-  set {
-    name  = "master.resources.requests.memory"
-    value = "128Mi"
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "redis"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "redis"
+        }
+      }
+
+      spec {
+        container {
+          image = "redis:alpine3.18"
+          name  = "redis"
+
+          volume_mount {
+            mount_path = "/data"
+            name       = "redis-storage"
+          }
+
+          resources {
+            requests = {
+              cpu    = "250m"
+              memory = "256Mi"
+            }
+            limits = {
+              cpu    = "500m"
+              memory = "512Mi"
+            }
+          }
+        }
+
+        volume {
+          name = "redis-storage"
+
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim.redis_pvc.metadata[0].name
+          }
+        }
+      }
+    }
+  }
+}
+
+# Persistent Volume Claim para PostgreSQL
+resource "kubernetes_persistent_volume_claim" "postgres_pvc" {
+  metadata {
+    name      = "postgres-pvc"
+    namespace = kubernetes_namespace.databases.metadata[0].name
   }
 
-  set {
-    name  = "master.resources.requests.cpu"
-    value = "200m"
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = "1Gi"
+      }
+    }
+  }
+}
+
+# Persistent Volume Claim para Redis
+resource "kubernetes_persistent_volume_claim" "redis_pvc" {
+  metadata {
+    name      = "redis-pvc"
+    namespace = kubernetes_namespace.databases.metadata[0].name
   }
 
-  set {
-    name  = "master.resources.limits.memory"
-    value = "512Mi"
-  }
-
-  set {
-    name  = "master.resources.limits.cpu"
-    value = "500m"
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = "1Gi"
+      }
+    }
   }
 }
