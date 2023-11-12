9011df83a41777e10d9839585a6da66bd0964072
Pedro Duarte <pedro.ti@live.com>
2023-11-12 12:21:36 -0300

    feat: add documentation

--- main.tf
+++ main.tf
@@ -5,5 +5,5 @@ provider "kubernetes" {
 
 # Load Data - Configmap para realizar carga de dados
 resource "kubernetes_manifest" "configmap_loaddata" {
-  manifest = yamldecode(file("${path.module}/jobs/configmap-load-data.yaml"))
+  manifest = yamldecode(file("${path.module}/configmaps/configmap-load-data.yaml"))
 }
